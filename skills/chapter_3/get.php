9773dfe770e5e258594a2e3caa6ff494dadc6191
Alexander86528 <74484953+Alexander86528@users.noreply.github.com>
2021-01-05 11:25:05 +0700

    Add files via upload

---
+++ skills/chapter_3/get.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>metod get</title>
+</head>
+<body>
+    <pre><?php
+            echo 'Metod GET' . PHP_EOL;
+            var_dump($_GET);
+        ?>
+    </pre>
+    <a href="/skills/chapter_3/get.php?sort=asc">сортировать</a>
+</body>
+</html>
