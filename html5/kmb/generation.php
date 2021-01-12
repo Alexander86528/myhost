dfe300117f2f67df82a41ef5ba597fc507417908
Alexander M <dl800259@gmail.com>
2021-01-12 18:10:49 +0700

    lesson

--- html5/kmb/generation.php
+++ html5/kmb/generation.php
@@ -1,12 +1,15 @@
+<?php
+    
+?>
 <!DOCTYPE html>
-<html lang="ru">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Generation</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Generator of progression</title>
 </head>
 <body>
-    <p>Generator of progression</p>
-    
+    <h4>Generator of progression</h4>
 
 </body>
 </html>
