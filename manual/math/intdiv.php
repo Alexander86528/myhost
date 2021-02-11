944720e9cd40df6bd8140b8af34b24f3f60456cb
Alexander M <dl800259@gmail.com>
2021-02-11 21:03:06 +0700

    math

---
+++ manual/math/intdiv.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+intdiv — Целочисленное деление.
+intdiv ( int $num1 , int $num2 ) : int
+Возвращает целочисленное деление num1 на num2.
+*/
+echo intdiv(3, 2) . PHP_EOL;
+echo intdiv(-3, 2) . PHP_EOL;
+echo intdiv(3, -2) . PHP_EOL;
+echo intdiv(-3, -2) . PHP_EOL;
+
+echo PHP_INT_MAX . PHP_EOL;
+echo PHP_INT_MIN . PHP_EOL;
+
+?>
