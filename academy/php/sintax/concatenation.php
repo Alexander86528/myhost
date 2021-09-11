fcb6aee62ac13fb718ba9399e3e1ef5154b7e82e
Alexander86528 <dl800259@gmail.com>
2021-09-11 12:35:59 +0700

    delete

--- academy/php/sintax/concatenation.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$favorite_color = "green";
-$favorite_number = "50";
-
-$united_string = "Мой любимый цвет " . $favorite_color . ", моё любимое число " . $favorite_number . "." . '<br>';
-
-print($united_string);
