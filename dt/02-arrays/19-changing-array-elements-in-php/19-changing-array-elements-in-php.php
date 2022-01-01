7d633867731e2427255cbc751ab73d78b9de65a9
Alexander86528 <dl800259@gmail.com>
2022-01-01 19:42:26 +0700

    php task

---
+++ dt/02-arrays/19-changing-array-elements-in-php/19-changing-array-elements-in-php.php
@@ -0,0 +1,19 @@
+<?php
+  error_reporting(E_ALL);
+  ini_set('display_errors', 'on');
+  mb_internal_encoding('UTF-8');
+
+  include 'task-01.php'
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>19. Изменение элементов массива в php</title>
+</head>
+<body>
+  
+</body>
+</html>
---
+++ dt/02-arrays/index.php
@@ -0,0 +1,19 @@
+<?php
+  error_reporting(E_ALL);
+  ini_set('display_errors', 'on');
+  mb_internal_encoding('UTF-8');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Arrays</title>
+</head>
+<body>
+  <h1>Массивы</h1>
+  <h2><a href="20-filling-arrays-in-php/filling-arrays-in-php.php">20. Заполнение массивов</a></h2>
+  <h2><a href="21-keys-from-variables-in-PHP/keys-from-variables-in-php.php">21. Ключи из переменных в php</a></h2>
+</body>
+</html>
