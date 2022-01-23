dd9a4af798e91f392abb34dd7bb7ad57fe73ed35
Alexander86528 <dl800259@gmail.com>
2022-01-23 14:18:44 +0700

    number.php и string.php примеры с metanit.com

---
+++ metanit/php/number.php
@@ -0,0 +1,10 @@
+<?php
+$num_10 = 28;
+$num_2 = 0b11100;
+$num_8 = 034;
+$num_16 = 0x1c;
+
+
+echo "num_2 = $num_2<br>";
+echo "num_8 = $num_8<br>";
+echo "num_16 = $num_16<br>";
---
+++ metanit/php/string.php
@@ -0,0 +1,8 @@
+<?php
+$a = 10;
+$b = 5;
+
+$result = "$a + $b <br>";
+echo $result;
+$result = '$a + $b <br>';
+echo $result;
