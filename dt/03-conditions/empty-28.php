e220ef8d617085369e043589b7356be11910f7ec
Alexander86528 <dl800259@gmail.com>
2022-02-01 15:31:05 +0700

    Для проверки

---
+++ dt/03-conditions/empty-28.php
@@ -0,0 +1,16 @@
+<?php
+echo "<a href='index.php'>03-conditions, index.php</a>";
+echo '<br>';
+// $test = false;
+// $test = '';
+// $test = 0;
+// $test = '0';
+$test = null;
+
+if (empty($test)) {
+  echo '<strong style="color:red;">Пусто</strong> <br>';
+} else {
+  echo 'Не пусто <br>';
+}
+
+var_dump($test);
