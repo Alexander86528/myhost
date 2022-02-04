ad2b208bd2070f854a933216316ac7085ac10fb2
Alexander86528 <dl800259@gmail.com>
2022-02-04 08:47:45 +0700

    Пример для git

--- dt/09-string-formation/inserting-variables-in-php-118.php
+++ dt/09-string-formation/inserting-variables-in-php-118.php
@@ -1,12 +1,25 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Вставка переменной в PHP</title>
-</head>
-<body>
-  
-</body>
-</html>
+<?php
+echo '<p>examle 1<br>вставка в строку переменной</p>';
+$str = 'bbb';
+
+echo 'aaa' . ' ' . $str . ' ' . 'ccc' . '<br>';
+echo "aaa $str ccc \n";
+
+
+echo '<p>task 1 <br>упростить код</p>';
+$user = 'name';
+
+echo 'hello' . ' ' . $user . '<br>';
+echo "hello $user \n";
+
+echo '<p>example 2<br>Вставка в строку элемента массива.</p>';
+$arr = ['b', 'c', 'd', ];
+
+echo 'a' . ' ' . $arr[0] . ' ' . $arr[1] . ' ' . $arr[2] . ' ' . 'e' . '<br>';
+echo "a $arr[0] $arr[1] $arr[2] e \n";
+
+echo '<p>task 2<br>Упростить код.</p>';
+$arr = ['1', '3', '5', ];
+
+echo '0' . ' ' . $arr[0] . ' ' . '2' . ' ' . $arr[1] . ' ' . '4' . ' ' . $arr[2] . '<br>';
+echo "0 $arr[0] 2 $arr[1] 4 $arr[2] \n";
