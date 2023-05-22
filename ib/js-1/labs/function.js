f4ab2a9f24a58c5166a780ed89c60fe9b9c3663e
kub060522 <sg01.04.21m31@gmail.com>
2023-05-22 10:14:55 +0700

    misc

--- ib/js-1/labs/function.js
+++ ib/js-1/labs/function.js
@@ -1,5 +1,21 @@
-function sayHello() {
-  document.write("Hello world!");
+function sayHello(name, sign) {
+  name = name || "Guest";
+  sign = sign || "...";
+  document.write("Hello " + name + sign + "<br>");
 }
 
+sayHello("John", "!");
+let n = "Mike";
+sayHello(n);
 sayHello();
+
+function power(base, exp) {
+  let result = 1;
+
+  for (let cnt = 0; cnt < exp; cnt++) {
+    result *= base;
+  }
+  return result;
+}
+ 
+document.write(power(2, 3));
