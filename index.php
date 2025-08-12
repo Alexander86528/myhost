e4d63f2b5e88c43d4e2bdf9ea51a81536a527db7
kub060522 <sg01.04.21m31@gmail.com>
2025-08-12 23:01:39 +0700

    Переименование

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
-echo 'Hello, world!' . PHP_EOL;
-phpinfo();
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+mb_internal_encoding('UTF-8');
+
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>index-html</title>
+</head>
+<body>
+  <ol>
+    <li><a href="/dt/the-basics-01">the basics 1-14</a></li>
+    <li><a href="/dt/02-arrays">arrays 15-21</a></li>
+    <li><a href="/dt/03-conditions">conditions 22-36</a></li>
+    <li><a href="/dt/04-cycles">cycles 37-61</a></li>
+    <li><a href="/dt/05-multidimensionality">multidimensionality 62-66</a></li>
+    <li><a href="/dt/06-standard-functions">standard functions 67-71</a></li>
+    <li><a href="/dt/07-custom-functions">custom function 72-83</a></li>
+    <li><a href="/dt/08-regular-expressions">regular expressions 84-117</a></li>
+    <li><a href="/dt/09-string-formation">string formation 118-126</a></li>
+  </ol>
+</body>
+</html>
