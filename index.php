5f77e56e237597b4b60d72bd58f1c1cf82822c21
kub060522 <sg01.04.21m31@gmail.com>
2025-08-12 17:37:40 +0700

    Добавляет вывод числа в index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-echo 'Hello, world!' . PHP_EOL;
+echo "Hello, PHP! <br>";
+/*
+$day = strftime('%d');
+$mon = strftime('%B');
+$year = strftime('%Y');
+echo $day, $mon, $year;
+*/
+echo strftime('%d %B %Y, %A');
+echo "<br";
 phpinfo();
